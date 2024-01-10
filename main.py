d15f11d36548d390ccfe5fe956eb3eeaba0d461d
2b07.2214621.yadanaraung <yadanarraung27@gmail.com>
2024-01-10 21:49:53 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Name: Lim Yu Yang Ian, Yadanar Aung
+# Admin No.: , 2214621
+# Class: DAAA/FT/2B/07
+
+# ------------------
+# Imports
+# ------------------
+
+
+# ------------------
+# Main Program
+# ------------------
